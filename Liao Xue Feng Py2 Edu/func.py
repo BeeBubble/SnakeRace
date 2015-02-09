5396c067db6159c6587e54f320735722e071a575
Shawn <fengxiaojun1990@qq.com>
2015-02-09 23:12:04 +0800

    廖雪峰的python傻瓜入门 function

--- Liao Xue Feng Py2 Edu/func.py
+++ Liao Xue Feng Py2 Edu/func.py
@@ -19,7 +19,7 @@ def my_abs(x):
 
 print my_abs(-100)
 print my_abs(2.2)
-print my_abs('a')
+# print my_abs('a')
 
 
 def nop():
@@ -27,3 +27,84 @@ def nop():
 
 
 print nop()
+
+import math
+
+
+def move(x, y, step, angle=0):
+    nx = x + step * math.cos(angle)
+    ny = y - step * math.sin(angle)
+    return nx, ny
+
+
+x, y = move(100, 100, 60, math.pi / 6)
+print x, y
+
+# 默认参数的坑
+def add_end(L=[]):
+    L.append('End')
+    return L
+
+
+print add_end()
+print add_end()
+
+
+def add_end2(L=None):
+    if L is None:
+        L = []
+    L.append('End')
+    return L
+
+
+print add_end2()
+print add_end2()
+
+# 可变参数
+def calc(*numbers):
+    sum = 0
+    for i in numbers:
+        sum += i
+    return sum
+
+
+print calc(1, 2, 3)
+
+num1 = [1, 2, 3]
+num2 = [4, 5, 6]
+print calc(*num1)
+print calc(*num2)
+
+
+# 关键字参数
+def person(name, age, **kw):
+    print 'name:', name, 'age:', age, 'other:', kw
+
+
+person('Michael', 30)
+person('Bob', 35, city='Beijing')
+person('Adam', 45, gender='M', job='Engineer')
+
+kw = {'city': 'Beijing', 'job': 'Engineer'}
+person('Jack', 24, city=kw['city'], job=kw['job'])
+person('Jack', 24, **kw)
+
+# 参数组合
+
+
+def func(a, b, c=0, *args, **kw):
+    print 'a =', a, 'b =', b, 'c =', c, 'args =', args, 'kw =', kw
+
+
+func(1, 2)
+func(1, 2, 3)
+func(1, 2, 3, 'a', 'b')
+func(1, 2, 3, 'a', 'b', x=99)
+
+args1 = (1, 2, 3, 4)
+kw1 = {'d': 'b'}
+
+func(*args1, **kw1)
+
+# 递归函数
+
