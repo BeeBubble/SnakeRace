691bcc0c3914f35962d517ff86120803b2c1bd4f
shawn <happyshawn88@gmail.com>
2015-02-11 17:05:49 +0800

    廖雪峰的python傻瓜入门 sort

--- Liao Xue Feng Py2 Edu/sort.py
+++ Liao Xue Feng Py2 Edu/sort.py
@@ -15,14 +15,15 @@ def reverse_cmp(x, y):
 print sorted([55, 79, 89, 74, 7468, 64, 4], reverse_cmp)
 
 
-def cmp_ignore_case(s1,s2):
+def cmp_ignore_case(s1, s2):
     s1 = s1.upper()
     s2 = s2.upper()
-    if s1<s2:
+    if s1 < s2:
         return -1
-    if s1>s2:
+    if s1 > s2:
         return 1
     return 0
+
 
 print sorted(['bob', 'about', 'Zoo', 'Credit'])
 print sorted(['bob', 'about', 'Zoo', 'Credit'], cmp_ignore_case)
